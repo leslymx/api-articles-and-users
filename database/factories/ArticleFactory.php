c20f4b866050879204afa876932e2e46f72d3e31
Lesly Perez <lkpsmx@gmail.com>
2021-12-04 22:44:44 -0600

    chore: db

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -18,7 +18,7 @@ class ArticleFactory extends Factory
             'SKU' => Str::random(8),
             'title' => $this->faker->sentence,
             'content' => $this->faker->sentence,
-            'user_id' => rand(5, 95),
+            'user_id' => random_bytes(2),
             'cover' => $this->faker->imageUrl(),
             'like' => random_int(1, 5)
         ];
